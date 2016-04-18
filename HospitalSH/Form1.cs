b5870b0e2cc46efdeb871588f69f4f2b00f05e6c
MagicLizi <407202761@qq.com>
2016-04-19 01:20:07 +0800

    first commit

---
+++ HospitalSH/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Hostpital;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalSH
+{
+    public partial class Form1 : Form
+    {
+        Sensor s;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "打开数据采集")
+            {
+                //直接读取出字符串
+                string host = System.IO.File.ReadAllText(@"sensorConfig.txt");
+
+                s = new Sensor();
+                s.mainForm = this;
+                s.ConnectToSensor(host, 502);
+                s.BeginTick();
+                button1.Text = "关闭数据采集";
+            }
+            else
+            {
+                s.CloseSensor();
+                button1.Text = "打开数据采集";
+            }
+        }
+
+        public void RefreshTextShow(string showContent,string sensorInfos)
+        {
+            richTextBox1.Text = richTextBox1.Text + showContent +"\r\n";
+            richTextBox1.Select(richTextBox1.Text.Length, 1);
+            richTextBox1.ScrollToCaret();
+
+            string p1 = @"softred.png";
+            string p2 = @"softred.png";
+            string p3 = @"softred.png";
+            string p4 = @"softred.png";
+            string p5 = @"softred.png";
+            label6.Text = "有人";
+            label7.Text = "有人";
+            label8.Text = "有人";
+            label9.Text = "有人";
+            label10.Text = "有人";
+            if (sensorInfos[0].ToString() == "0")
+            {
+                p1 = @"softgreen.png";
+                label6.Text = "无人";
+            }
+            if (sensorInfos[1].ToString() == "0")
+            {
+                p2 = @"softgreen.png";
+                label7.Text = "无人";
+            }
+            if (sensorInfos[2].ToString() == "0")
+            {
+                p3 = @"softgreen.png";
+                label8.Text = "无人";
+            }
+            if (sensorInfos[3].ToString() == "0")
+            {
+                p4 = @"softgreen.png";
+                label9.Text = "无人";
+            }
+            if (sensorInfos[4].ToString() == "0")
+            {
+                p5 = @"softgreen.png";
+                label10.Text = "无人";
+            }
+
+
+            pictureBox1.Image = Image.FromFile(p1);
+            pictureBox2.Image = Image.FromFile(p2);
+            pictureBox3.Image = Image.FromFile(p3);
+            pictureBox4.Image = Image.FromFile(p4);
+            pictureBox5.Image = Image.FromFile(p5);
+
+        }
+
+    }
+}
---
+++ HospitalSH/Sensor.cs
@@ -0,0 +1,149 @@
+﻿using HospitalSH;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Hostpital
+{
+    class Sensor
+    {
+        public Form1 mainForm;
+
+        Socket socket;
+
+        static int id = 0;
+
+        static object _object = new object();
+
+        System.Timers.Timer tmr;
+        public void ConnectToSensor(string host, int port)
+        {
+
+            IPAddress myIP = IPAddress.Parse(host);
+            IPEndPoint hostEP = new IPEndPoint(myIP, port);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(hostEP);
+            Console.WriteLine("连接传感器成功...");
+            Console.WriteLine();
+
+        }
+
+        public void CloseSensor()
+        {
+            tmr.Stop();
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+        }
+
+        public void BeginTick()
+        {
+            Console.WriteLine("开始监控轮询传感器数据...");
+            Console.WriteLine();
+            tmr = new Timer();
+            tmr.Interval = 2000;
+            tmr.Elapsed += new ElapsedEventHandler(tmr_Elapsed);
+            tmr.Start();
+        }
+
+        void tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("编号{0}：获取传感器信息...", id);
+            id++;
+            GetSensorInfo();
+        }
+
+        public void GetSensorInfo()
+        {
+            lock (_object)
+            {
+                byte[] sendCommand = new byte[12];
+                sendCommand[0] = 0x00;
+                sendCommand[1] = 0x01;
+                sendCommand[2] = 0x00;
+                sendCommand[3] = 0x00;
+                sendCommand[4] = 0x00;
+                sendCommand[5] = 0x06;
+                sendCommand[6] = 0xFF;
+                sendCommand[7] = 0x02;
+                sendCommand[8] = 0x00;
+                sendCommand[9] = 0xC8;
+                sendCommand[10] = 0x00;
+                sendCommand[11] = 0x08;
+                socket.Send(sendCommand);
+
+                byte[] result = new byte[1024];
+                int receiveLength = socket.Receive(result);
+                string sensorInfo = DecimalToBinary(result[9]);
+                ShowSensorInfo(sensorInfo);
+            }
+        }
+
+        public string DecimalToBinary(int decimalNum)
+        {
+            string binaryNum = Convert.ToString(decimalNum, 2);
+            if (binaryNum.Length < 8)
+            {
+                int length = binaryNum.Length;
+                for (int i = 0; i < 8 - length; i++)
+                {
+                    binaryNum = '0' + binaryNum;
+                }
+            }
+            return binaryNum;
+        }
+
+        // 0 断开 1闭合
+        public void ShowSensorInfo(string sensorInfo)
+        {
+            Console.WriteLine("接收最新传感器二进制数据：{0}", sensorInfo);
+            string DI1 = "断开";
+            string DI2 = "断开";
+            string DI3 = "断开";
+            string DI4 = "断开";
+            string DI5 = "断开";
+            if (sensorInfo[0].ToString() == "1")
+            {
+                DI1 = "闭合";
+            }
+            if (sensorInfo[1].ToString() == "1")
+            {
+                DI2 = "闭合";
+            }
+            if (sensorInfo[2].ToString() == "1")
+            {
+                DI3 = "闭合";
+            }
+            if (sensorInfo[3].ToString() == "1")
+            {
+                DI4 = "闭合";
+            }
+            if (sensorInfo[4].ToString() == "1")
+            {
+                DI5 = "闭合";
+            }
+
+            string content = "编号" + id + "：获取传感器信息..."+"\r\n" +
+                             "接收最新传感器二进制数据：" + sensorInfo + "\r\n" +
+                             "DI1:" + DI1 + "\r\n" +
+                             "DI2:" + DI2 + "\r\n" +
+                             "DI3:" + DI3 + "\r\n" +
+                             "DI4:" + DI4 + "\r\n" +
+                             "DI5:" + DI5 + "\r\n";
+            mainForm.RefreshTextShow(content, sensorInfo);
+
+            Console.WriteLine("DI1:" + DI1);
+            Console.WriteLine("DI2:" + DI2);
+            Console.WriteLine("DI3:" + DI3);
+            Console.WriteLine("DI4:" + DI4);
+            Console.WriteLine("DI5:" + DI5);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
